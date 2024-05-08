320a4e8f69746010c0b375abc40e9de2e6fa0bfb
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-05-08 20:28:18 +0300

    Refactor App.java to include web crawling functionality and improve search functionality

--- src/invertedIndex/App.java
+++ src/invertedIndex/App.java
@@ -1,9 +1,7 @@
 package invertedIndex;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class App {
     public static void main(String args[]) throws IOException {
@@ -19,6 +17,7 @@ public class App {
         }
         index.num_files = fileList.length;
         index.files = fileList;
+        index.web_crawler("https://en.wikipedia.org/wiki/List_of_pharaohs");
         index.buildIndex();
         index.store("index");
         index.buildBiwordIndex();
@@ -26,31 +25,30 @@ public class App {
         // String test = "data should plain greatest comif";
         // System.out.println("Boolean Model result = \n" + index.find(test));
         // System.out.println("Boolean Model result = " + index.positionalIndex(test));
-index.web_crawler("https://en.wikipedia.org/wiki/List_of_pharaohs");
         // // Search
         // String phrase = "";
         // do {
-        //     System.out.print("Search Phrase: ");
-        //     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        //     phrase = in.readLine();
-        //     if (!phrase.isEmpty()) {
-        //         if (phrase.contains("\"")) {
-        //             phrase = phrase.replaceAll("\"(\\w+)\\s+(\\w+)\"", "$1_$2").toLowerCase();
-        //         }
-        //         System.out.println("Sending: " + phrase);
-        //         System.out.println("Result:\n" + index.find(phrase));
-        //     }
+        // System.out.print("Search Phrase: ");
+        // BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        // phrase = in.readLine();
+        // if (!phrase.isEmpty()) {
+        // if (phrase.contains("\"")) {
+        // phrase = phrase.replaceAll("\"(\\w+)\\s+(\\w+)\"", "$1_$2").toLowerCase();
+        // }
+        // System.out.println("Sending: " + phrase);
+        // System.out.println("Result:\n" + index.find(phrase));
+        // }
         // } while (!phrase.isEmpty());
 
         // // Positional Search
         // do {
-        //     System.out.print("Positional Search Phrase: ");
-        //     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        //     phrase = in.readLine();
-        //     if (!phrase.isEmpty()) {
-        //         System.out.println("Sending: " + phrase);
-        //         System.out.println("Result:\n" + index.positionalIndex(phrase));
-        //     }
+        // System.out.print("Positional Search Phrase: ");
+        // BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        // phrase = in.readLine();
+        // if (!phrase.isEmpty()) {
+        // System.out.println("Sending: " + phrase);
+        // System.out.println("Result:\n" + index.positionalIndex(phrase));
+        // }
         // } while (!phrase.isEmpty());
     }
 }
